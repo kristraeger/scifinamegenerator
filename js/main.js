324aaae8e8fa92f774f8e86ecf22476811aef758
Kristina Traeger <kristina@traeger.co>
2016-01-29 15:34:21 -0800

    looping

--- js/main.js
+++ js/main.js
@@ -1,13 +1,5 @@
 
-var scifiName = function () {
-
-	// create name from data set using name_generator.js
-	var matchVal = generate_name('egyptian').toLowerCase()
-	console.log(matchVal)
-
-	// create array from all characters of match value
-	var matchSplit = matchVal.split("")
-	console.log(matchSplit)
+var scifiName = function  () {
 
 	// convert input to lowercase
 	var inputVal = document.getElementById('inputVal').value.toLowerCase()
@@ -17,6 +9,41 @@ var scifiName = function () {
 	var inputSplit = inputVal.split("")
 	console.log(inputSplit)
 
-	// 
+	// generate name using name_generator.js
+	var matchVal = generate_name('egyptian').toLowerCase()
+	console.log(matchVal)
 
-}
+	// create array from all characters of match value
+	var matchSplit = matchVal.split("")
+	console.log(matchSplit)
+
+	//create array to store matching values
+	var matchArr = []
+	
+	//loop through sorted arrays and add duplicates to match array
+	for (var i= 0; i < inputSplit.length; i++) {
+		for(var j =0; j < matchSplit.length; j++)
+			if (inputSplit[i] == matchSplit[j]) {
+				matchArr.push(inputSplit[i]);
+			}
+	}
+
+	var sorted_matchArr = matchArr.sort()
+	console.log(sorted_matchArr)
+
+	var uniqueArr = []
+	//loop through array and find unique values
+	for( i=0; i< sorted_matchArr.length; i++) {
+		if( sorted_matchArr[i] !== sorted_matchArr [i+1])
+			uniqueArr.push(sorted_matchArr[i])
+	}
+
+	console.log(uniqueArr)
+
+	if (uniqueArr.length > 3) {
+		var result = matchVal
+	}
+
+	console.log(result)
+
+} // ./ scifiName
