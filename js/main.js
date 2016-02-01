d1e8191e736cfa522b098f59f3ec7fdca86ec026
Kristina Traeger <kristina@traeger.co>
2016-01-31 23:49:06 -0800

    name generator

--- js/main.js
+++ js/main.js
@@ -6,44 +6,60 @@ var scifiName = function  () {
 	console.log(inputVal)
 
 	// create array from all characters of input value
-	var inputSplit = inputVal.split("")
+	var inputSplit = []
+	inputSplit = inputVal.split("")
+	var lastChInput = inputSplit.length -1 
 	console.log(inputSplit)
 
-	// generate name using name_generator.js
-	var matchVal = generate_name('egyptian').toLowerCase()
-	console.log(matchVal)
-
-	// create array from all characters of match value
-	var matchSplit = matchVal.split("")
-	console.log(matchSplit)
-
-	//create array to store matching values
-	var matchArr = []
 	
-	//loop through sorted arrays and add duplicates to match array
-	for (var i= 0; i < inputSplit.length; i++) {
-		for(var j =0; j < matchSplit.length; j++)
-			if (inputSplit[i] == matchSplit[j]) {
-				matchArr.push(inputSplit[i]);
+	var matchVal
+	var matchSplit = []
+
+	do {
+
+		do{
+			do {
+			// generate name using name_generator.js
+			matchVal = generate_name('egyptian').toLowerCase()
+			console.log(matchVal)
+
+			// create array from all characters of match value
+			matchSplit = matchVal.split("")
+			lastChMatch = matchSplit.length -1 
+			console.log(matchSplit)
+			} while ( inputSplit[0] !== matchSplit[0])
+		} while (inputSplit[lastChInput] !== matchSplit[lastChMatch])
+
+		var matchArr = []
+		var sorted_matchArr =[]
+		
+		//loop through sorted arrays and add duplicates to match array
+		for (var i= 0; i < inputSplit.length; i++) {
+			for(var j =0; j < matchSplit.length; j++)
+				if (inputSplit[i] == matchSplit[j]) {
+					matchArr.push(inputSplit[i]);
+				}
+		}
+
+		sorted_matchArr = matchArr.sort()
+		console.log(sorted_matchArr)
+		var uniqueArr = []
+
+		//loop through array and find unique values
+		for(var i=0; i < sorted_matchArr.length; i++) {
+			//if two values in sorted match array are not similar
+			if( sorted_matchArr[i+1] !== sorted_matchArr[i]) {
+				//check if value already exists in unique array
+				if(uniqueArr.indexOf(sorted_matchArr[i]) !== sorted_matchArr[i]){
+					uniqueArr.push(sorted_matchArr[i])
+				}
 			}
-	}
-
-	var sorted_matchArr = matchArr.sort()
-	console.log(sorted_matchArr)
-
-	var uniqueArr = []
-	//loop through array and find unique values
-	for( i=0; i< sorted_matchArr.length; i++) {
-		if( sorted_matchArr[i] !== sorted_matchArr [i+1])
-			uniqueArr.push(sorted_matchArr[i])
-	}
+		}
+	} while (uniqueArr.length < 3)
 
 	console.log(uniqueArr)
+	console.log(matchVal)
 
-	if (uniqueArr.length > 3) {
-		var result = matchVal
-	}
-
-	console.log(result)
+	return matchVal
 
 } // ./ scifiName
