f376f17fbecbb77617dcf0344e249152b907a959
Kristina Traeger <kristina@traeger.co>
2016-02-01 19:09:20 -0800

    bb8

--- js/main.js
+++ js/main.js
@@ -1,8 +1,27 @@
+function showBox () {
+	// show result box
+	document.getElementById("resultBox").style.display = "block"
+	//focus on input 
+	document.getElementById("inputVal").focus()
+	// hide intro
+	$(".intro").hide()
+	// // scroll to box
+	// $("html, body").animate({
+ //        scrollTop: $("#autoscroll").offset().top
+ //      	}, 400, "easeInCubic");
+}
 
 var scifiName = function  () {
 
+	//validate input 
+	if (document.getElementById("inputVal").value.trim() === ""){
+		document.getElementById("result").innerHTML = "please enter your first name in the search field above!"
+		showBox()
+		return
+	}
+
 	// convert input to lowercase
-	var inputVal = document.getElementById('inputVal').value.toLowerCase()
+	var inputVal = document.getElementById("inputVal").value.toLowerCase()
 	console.log(inputVal)
 
 	// create array from all characters of input value
@@ -17,18 +36,16 @@ var scifiName = function  () {
 
 	do {
 
-		do{
-			do {
-			// generate name using name_generator.js
-			matchVal = generate_name('egyptian').toLowerCase()
-			console.log(matchVal)
+		do {
+		// generate name using name_generator.js
+		matchVal = generate_name("data").toLowerCase()
+		console.log(matchVal)
 
-			// create array from all characters of match value
-			matchSplit = matchVal.split("")
-			lastChMatch = matchSplit.length -1 
-			console.log(matchSplit)
-			} while ( inputSplit[0] !== matchSplit[0])
-		} while (inputSplit[lastChInput] !== matchSplit[lastChMatch])
+		// create array from all characters of match value
+		matchSplit = matchVal.split("")
+		lastChMatch = matchSplit.length -1 
+		console.log(matchSplit)
+		} while ( inputSplit[0] !== matchSplit[0])
 
 		var matchArr = []
 		var sorted_matchArr =[]
@@ -60,6 +77,81 @@ var scifiName = function  () {
 	console.log(uniqueArr)
 	console.log(matchVal)
 
-	return matchVal
+	// show result in result box
+	document.getElementById("result").innerHTML = matchVal
+	document.getElementById("autoscroll").innerHTML = "Keep clicking for more results!"
+	showBox()
 
 } // ./ scifiName
+
+// BB8
+
+var $w = $( window ).width();
+var $dW = $('.bb8').css('width');
+$dW = $dW.replace('px', '');
+$dW = parseInt($dW);
+var $dPos = 0;
+var $dSpeed = 1;
+var $dMinSpeed = 1;
+var $dMaxSpeed = 4;
+var $dAccel = 1.04;
+var $dRot = 0;
+var $mPos = $w - $w/5;
+var $slowOffset = 120;
+var $movingRight = false;
+
+function moveDroid(){
+  if($mPos > $dPos + ($dW/4)){
+    // moving right
+    if(!$movingRight){
+      $movingRight = true;
+      $('.antennas').addClass('right');
+      $('.eyes').addClass('right');
+    }
+    if($mPos - $dPos > $slowOffset){
+      if($dSpeed < $dMaxSpeed){
+        // speed up
+        $dSpeed = $dSpeed * $dAccel;
+      }
+    } else if($mPos-$dPos < $slowOffset){
+      if($dSpeed > $dMinSpeed){
+        // slow down
+        $dSpeed = $dSpeed / $dAccel;
+      }
+    }
+    $dPos = $dPos + $dSpeed;
+    $dRot = $dRot + $dSpeed;
+  } else if($mPos < $dPos - ($dW/4)){
+    // moving left
+    if($movingRight){
+      $movingRight = false;
+      $('.antennas').removeClass('right');
+      $('.eyes').removeClass('right');
+    }
+    if($dPos - $mPos > $slowOffset){
+      if($dSpeed < $dMaxSpeed){
+        // speed up
+        $dSpeed = $dSpeed * $dAccel;
+      }
+    } else if($dPos - $mPos < $slowOffset){
+      if($dSpeed > $dMinSpeed){
+        // slow down
+        $dSpeed = $dSpeed / $dAccel;
+      }
+    }
+    $dPos = $dPos - $dSpeed;
+    $dRot = $dRot - $dSpeed;
+  } else { }
+  $('.bb8').css('left', $dPos);
+  $('.ball').css({ WebkitTransform: 'rotate(' + $dRot + 'deg)'});
+  $('.ball').css({ '-moz-transform': 'rotate(' + $dRot + 'deg)'});
+}
+
+setInterval(moveDroid, 10);
+
+$( document ).on( "mousemove", function( event ) {
+  $('h2').addClass('hide');
+  $mPos = event.pageX;
+  return $mPos;
+});
+
